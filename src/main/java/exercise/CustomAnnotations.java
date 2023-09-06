851425a96654748222ab72255431fee6addd8636
jacobvanlingen <je.lingen@portofrotterdam.com>
2023-09-06 19:57:18 +0200

    Add extra fields in CustomAnnotations

--- src/main/java/exercise/CustomAnnotations.java
+++ src/main/java/exercise/CustomAnnotations.java
@@ -9,6 +9,9 @@ package exercise;
 public class CustomAnnotations {
     private String foo;
     private boolean bar;
+    // 4. Does your code still work if you uncomment following fields?
+    // private long rab;
+    // private int oof;
 
     // 3.1 Remove constructor
     public CustomAnnotations(final String foo, final boolean bar) {
