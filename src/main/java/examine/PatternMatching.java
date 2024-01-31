a5e55187da9f3eca7e58db90c14dc4690e735e63
lingenj <je.lingen@portofrotterdam.com>
2024-01-31 16:34:27 +0100

    Improve code examples

--- src/main/java/examine/PatternMatching.java
+++ src/main/java/examine/PatternMatching.java
@@ -3,8 +3,6 @@ package examine;
 public class PatternMatching {
 
     public static void main(String[] args) {
-        var x = 32;
-
         Object o = 23; // any object
         String formatter = switch(o) {
             case Integer i -> String.format("int %d", i);
