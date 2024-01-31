06e330416589c348838282c5f5c386179af652c1
lingenj <je.lingen@portofrotterdam.com>
2024-01-31 16:10:19 +0100

    Improve code examples

--- src/main/java/examine/UseOfFunctionTwo.java
+++ src/main/java/examine/UseOfFunctionTwo.java
@@ -3,9 +3,11 @@ package examine;
 public class UseOfFunctionTwo {
 
     public static void main(String[] args) {
-        var x = add(1, 3);
-        var y = x + 100;
-        var z = (x != -1) ? x + y : 23;
+        var v = add(1, 3);
+        var w = v + 100;
+        var x = (v != -1) ? v + w : 23;
+        var y = x + 1000;
+        var z = x + 1000000;
     }
 
     public static int add(final int a, final int b) {
